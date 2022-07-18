1f7b5d5316bea73d01a7213188bd140bd29bf239
Julien <julien@gravityone.co.za>
2022-07-18 21:17:25 +0200

    URL calls added/fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 
 const request = require('request');
-
-// declare all servers to check
-const urls = [
-  "https://stackworx.io/",
-  "https://dev.vantage.run/health",
-  "https://status.gitlab.com/",
-]
 
 
 function App() {
 
-  const [servers, setServers] = useState([] as Array<any>);
+  const [servers, setServers] = useState([
+    {
+      url: "https://www.stackworx.io",
+      is_up: false,
+      last_update: "unknown",
+    },{
+      url: "https://status.gitlab.com/",
+      is_up: false,
+      last_update: "unknown",
+    },{
+      url: "https://dev.vantage.run/health",
+      is_up: false,
+      last_update: "unknown",
+    }
+  ] as any);
+  const [theme, setTheme] = useState("light" as String);
 
   useEffect(() => {
-    initServers();
+    let update_frequency_minutes = 1;
+    let update_frequency_seconds = update_frequency_minutes * 60;
+    let update_frequency_milliseconds = update_frequency_seconds * 1000;
+    console.log(update_frequency_milliseconds);
+    setInterval(() => {
+      checkServers();
+    }, update_frequency_milliseconds);
   }, []);
 
-  const initServers = () => {
-    let servers = [] as Array<any>;
-    urls.forEach(url => {
-      servers.push({
-        url: url,
-        is_up: false,
-        last_update: "unknown",
-      })
-    });
-    setServers(servers);
+
+  const checkServers = () => {
+    console.log("Running checkServers");
+    if(servers.length > 0) {
+      servers.map((server: any) => {
+        console.log("Server: " + server.url);
+        request.get(server.url, {method: 'GET'}, (error: any, response: any, body: any) => {
+          if(parseInt(response.statusCode) !== 200) {
+            console.log(error)
+            console.log("Server is down");
+            server.is_up = false;
+          }else{
+            console.log("Server is up");
+            server.is_up = true;
+          }
+          server.last_update = new Date().toLocaleString();
+          setServers([...servers]);
+        });
+      });
+    }else{
+      console.log("No servers to check");
+    }
   }
 
   return (
